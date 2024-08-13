03dc1e6f2d14ea57cf3976730f9a2f52ab671c18
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-12 23:41:27 -0500

    add banner

--- src/main/java/com/notifications/ms/NotificationsMsApplication.java
+++ src/main/java/com/notifications/ms/NotificationsMsApplication.java
@@ -1,13 +1,20 @@
 package com.notifications.ms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationsMsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(NotificationsMsApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(NotificationsMsApplication.class, args);
+		logger.info("Welcome to Notifications Microservice!");
+
 	}
 
 }
