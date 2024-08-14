36a85ec336748713734df7a62a0eedb2d10b1ab7
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-14 18:35:44 -0500

    add service

--- src/main/java/com/notifications/ms/model/EmailRequest.java
+++ src/main/java/com/notifications/ms/model/EmailRequest.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Email {
+public class EmailRequest {
     private String to;
     private String subject;
     private String body;
---
+++ src/main/java/com/notifications/ms/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.notifications.ms.service;
+
+import com.notifications.ms.model.EmailRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(EmailRequest emailRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailRequest.getTo());
+        message.setSubject(emailRequest.getSubject());
+        message.setText(emailRequest.getBody());
+
+        emailSender.send(message);
+    }
+}
